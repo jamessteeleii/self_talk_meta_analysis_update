70e362aa76ffd7f692640dcb9a8888617d64c3c1
James Steele <james.steele@solent.ac.uk>
2023-10-23 13:55:36 +0100

    Main model ran, began moderator models - figuring out how to derive priors

---
+++ R/functions.R
@@ -0,0 +1,458 @@
+# Read in and prepare data for effect size calculations and models
+read_prepare_data <- function(file) {
+  data <- read_csv(here("data", "Final data.csv")) %>%
+    mutate_at(c(2:8, 30:32, 34:37), as.factor) %>%
+    clean_names() %>%
+
+    # add in pre-post correlation assumption
+    mutate(ri = 0.7) %>%
+
+    # add calculation of pooled baseline standard deviations
+    mutate(pre_sd_pool = sqrt(((n_st - 1) * pre_sd_st ^ 2 +
+                                 (n_con - 1) * pre_sd_con ^ 2
+    ) /
+      (n_st + n_con - 2)),
+    post_sd_pool = sqrt(((n_st - 1) * post_sd_st ^ 2 +
+                           (n_con - 1) * post_sd_con ^ 2
+    ) /
+      (n_st + n_con - 2)))
+}
+
+# Calculate all effect sizes and recombine data
+calculate_effect_sizes <- function(data) {
+  ### For effects where increase is good
+  data_increase <- subset(data, improvement == "Increase")
+
+  # Calculate pre-post control effect sizes
+  data_increase_ppc <-
+    subset(data_increase, study_design == "between")
+
+  data_increase_ppc_st <- escalc(
+    measure = "SMCR",
+    m1i = post_m_st,
+    m2i = pre_m_st,
+    sd1i = pre_sd_pool,
+    ni = n_st,
+    ri = ri,
+    data = data_increase_ppc
+  )
+  data_increase_ppc_con <- escalc(
+    measure = "SMCR",
+    m1i = post_m_con,
+    m2i = pre_m_con,
+    sd1i = pre_sd_pool,
+    ni = n_con,
+    ri = ri,
+    data = data_increase_ppc
+  )
+
+  data_increase_ppc$yi <-
+    (data_increase_ppc_st$yi - data_increase_ppc_con$yi)
+  data_increase_ppc$vi <-
+    (data_increase_ppc_st$vi + data_increase_ppc_con$vi)
+
+  # Calculate post only effect sizes
+  data_increase_post <-
+    subset(data_increase, study_design == "between-post")
+
+  data_increase_post$post_sd_st <-
+    replmiss(data_increase_post$post_sd_st,
+             with(data_increase_post, post_sd_con))
+
+  data_increase_post <- escalc(
+    measure = "SMD",
+    m1i = post_m_st,
+    m2i = post_m_con,
+    sd1i = post_sd_st,
+    sd2i = post_sd_con,
+    n1i = n_st,
+    n2i = n_con,
+    data = data_increase_post
+  )
+
+  # Calculate pre-post effect sizes
+  data_increase_pp <-
+    subset(data_increase, study_design == "pre-post")
+
+  data_increase_pp <- escalc(
+    measure = "SMCR",
+    m1i = post_m_st,
+    m2i = pre_m_st,
+    sd1i = pre_sd_st,
+    ni = n_st,
+    ri = ri,
+    data = data_increase_pp
+  )
+
+  ### For effects where decrease is good
+  data_decrease <- subset(data, improvement == "Decrease")
+
+  # Calculate pre-post control effect sizes
+  data_decrease_ppc <-
+    subset(data_decrease, study_design == "between")
+
+  data_decrease_ppc_st <- escalc(
+    measure = "SMCR",
+    m1i = pre_m_st,
+    m2i = post_m_st,
+    sd1i = pre_sd_pool,
+    ni = n_st,
+    ri = ri,
+    data = data_decrease_ppc
+  )
+  data_decrease_ppc_con <- escalc(
+    measure = "SMCR",
+    m1i = pre_m_con,
+    m2i = post_m_con,
+    sd1i = pre_sd_pool,
+    ni = n_con,
+    ri = ri,
+    data = data_decrease_ppc
+  )
+
+  data_decrease_ppc$yi <-
+    (data_decrease_ppc_st$yi - data_decrease_ppc_con$yi)
+  data_decrease_ppc$vi <-
+    (data_decrease_ppc_st$vi + data_decrease_ppc_con$vi)
+
+  # Calculate post only effect sizes
+  data_decrease_post <-
+    subset(data_decrease, study_design == "between-post")
+
+  data_decrease_post$post_sd_st <-
+    replmiss(data_decrease_post$post_sd_st,
+             with(data_decrease_post, post_sd_con))
+
+  data_decrease_post <- escalc(
+    measure = "SMD",
+    m1i = post_m_con,
+    m2i = post_m_st,
+    sd1i = post_sd_st,
+    sd2i = post_sd_con,
+    n1i = n_con,
+    n2i = n_st,
+    data = data_decrease_post
+  )
+
+  # Calculate pre-post effect sizes
+  data_decrease_pp <-
+    subset(data_decrease, study_design == "pre-post")
+
+  data_decrease_pp <- escalc(
+    measure = "SMCR",
+    m1i = pre_m_st,
+    m2i = post_m_st,
+    sd1i = pre_sd_st,
+    ni = n_st,
+    ri = ri,
+    data = data_decrease_pp
+  )
+
+
+  ### Combine all data
+  data <-
+    rbind(
+      data_decrease_post,
+      data_decrease_pp,
+      data_decrease_ppc,
+      data_increase_post,
+      data_increase_pp,
+      data_increase_ppc
+    )
+}
+
+# Misc functions
+make_plot_tiff <- function(plot, width, height, path) {
+  ggsave(
+    path,
+    plot,
+    width = width,
+    height = height,
+    device = "tiff",
+    dpi = 300
+  )
+
+}
+
+get_tidy_model <- function(model) {
+  tidy(model)
+}
+
+# Setup rstan to run quicker
+rstan_setup <- function() {
+  rstan_options(auto_write = TRUE)
+  options(mc.cores = parallel::detectCores() - 1)
+}
+
+# Priors
+intercept_prior <- function() {
+  intercept_prior <-
+    prior("student_t(3, 0.48, 0.05)", class = "Intercept")
+}
+
+prior_plot <- function() {
+  ggplot() +
+    stat_function(
+      data = data.frame(x = c(-2, 2)),
+      aes(x),
+      fun = dstudent_t,
+      n = 101,
+      args = list(
+        df = 3,
+        mu = 0.48,
+        sigma = 0.05,
+        log = FALSE
+      ),
+      color = "black"
+    ) +
+    geom_point(aes(y = 0, x = 0.48)) +
+    geom_errorbarh(aes(y = 0, xmin = 0.38, xmax = 0.58)) +
+    labs(
+      x = "Prior for model intercept (Standardised Mean Difference)" ,
+      y = "PDF",
+      title = "Visualisation of prior distribution",
+      subtitle = "Taken from main model intercept estimate from Hatzigeorgiadis et al., (2011)"
+    ) +
+    scale_x_continuous(limits = c(-2, 2), breaks = c(-2,-1, 0, 1, 2)) +
+    theme_classic() +
+    theme(panel.border = element_rect(fill = NA))
+}
+
+# Models
+fit_main_model <- function(data, prior) {
+  main_model <-
+    brm(
+      yi | se(sqrt(vi)) ~ 1 + (1 | study / experiment / group / effect),
+      data = data,
+      prior = prior,
+      chains = 4,
+      cores = 4,
+      seed = 1988,
+      warmup = 2000,
+      iter = 8000,
+      control = list(adapt_delta = 0.99)
+    )
+}
+
+plot_main_model <- function(data, model) {
+  # Get study level draws and summarise
+  study_draws <- model %>%
+    spread_draws(b_Intercept, r_study[study,]) %>%
+    mutate(b_Intercept = b_Intercept + r_study,
+           study = as.factor(study)) %>%
+    select(.chain, .iteration, .draw, b_Intercept, study) %>%
+    merge(data[, 1:2], by = "study")
+
+  study_summary <- group_by(study_draws, label) %>%
+    mean_qi(b_Intercept) %>%
+    mutate(b_Intercept = b_Intercept,
+           .lower = .lower,
+           .upper = .upper)
+
+  # Get pooled estimate draws and summarise
+  pooled_draws <- model %>%
+    spread_draws(b_Intercept) %>%
+    mutate(study = "Posterior Pooled Estimate",
+           label = "Posterior Pooled Estimate")
+
+  pooled_summary <- group_by(pooled_draws, label) %>%
+    mean_qi(b_Intercept) %>%
+    mutate(b_Intercept = b_Intercept,
+           .lower = .lower,
+           .upper = .upper)
+
+  # Get prediction interval
+  nd <- data.frame(study = "new", vi = 0)
+
+  pred_int_data <- posterior_predict(
+    object = model,
+    newdata = nd,
+    re_formula = NULL,
+    allow_new_levels = TRUE,
+    sample_new_levels = "gaussian"
+  )
+
+  pred_int_data <- median_qi(pred_int_data) %>%
+    mutate(label = as.character("Posterior Pooled Estimate"))
+
+  # Dataframe for the prior to plot
+  prior_plot <-
+    data.frame(b_Intercept = rstudent_t(
+      n = 1e6,
+      df = 3,
+      mu = 0.48,
+      sigma = 0.05
+    ),
+    label = "Prior (Hatzigeorgiadis et al., 2011)")
+
+  # Forest plot of study level estimates
+  forest_study <- ggplot(aes(x = b_Intercept,
+                             y = reorder(label, b_Intercept)),
+                         data = study_draws) +
+
+    # Add band for prediction interval
+    annotate(
+      "rect",
+      xmin = pred_int_data$ymin,
+      xmax = pred_int_data$ymax,
+      ymin = -Inf,
+      ymax = Inf,
+      alpha = .1,
+      fill = "black"
+    ) +
+
+    # Add ref line at zero
+    geom_vline(xintercept = 0, linetype = 2) +
+
+    scale_y_discrete() +
+
+    # Add densities and point intervals
+    geom_density_ridges(
+      fill = "darkgrey",
+      rel_min_height = 0.01,
+      col = NA,
+      scale = 1,
+      alpha = 0.8
+    ) +
+    stat_pointinterval(point_interval = mean_qi,
+                       .width = .95,
+                       size = 0.75) +
+
+    # Add individual study data
+    geom_point(
+      data = subset(data, !is.na(yi)),
+      aes(x = yi, y = label),
+      position = position_nudge(y = -0.1),
+      shape = "|"
+    ) +
+
+    # Add text and labels
+    geom_text(
+      data = mutate_if(study_summary,
+                       is.numeric, round, 2),
+      aes(
+        label = glue::glue("{b_Intercept} [{.lower}, {.upper}]"),
+        x = 4
+      ),
+      hjust = "inward",
+      size = 3
+    ) +
+    labs(
+      x = element_blank(),
+      # summary measure
+      y = element_blank(),
+      title = "Study level estimates",
+      subtitle = "Posterior distributions, mean and 95% quantile intervals, individual effects (ticks), and 95% prediction interval (grey band)"
+    ) +
+    scale_x_continuous(limits = c(-2, 4),
+                       breaks = c(-2,-1, 0, 1, 2, 3, 4)) +
+    theme_classic() +
+    theme(panel.border = element_rect(fill = NA),
+          plot.subtitle = element_text(size = 6))
+
+  # Combine prior dataframe with pooled draws
+  prior_posterior <- rbind(pooled_draws[, c(4, 6)], prior_plot) %>%
+    mutate(label = factor(
+      label,
+      levels = c(
+        "Prior (Hatzigeorgiadis et al., 2011)",
+        "Posterior Pooled Estimate"
+      )
+    ))
+
+  # Plot showing updating of posterior estimate
+  posterior_update <- ggplot(data = prior_posterior,
+                             aes(x = b_Intercept,
+                                 color = label, fill = label)) +
+
+    # Add reference line at zero
+    geom_vline(xintercept = 0, linetype = 2) +
+
+    # Add densities
+    geom_density(
+      rel_min_height = 0.01,
+      col = NA,
+      scale = 1,
+      alpha = 0.5
+    ) +
+
+    # Add text and labels
+    geom_text(
+      data = mutate_if(pooled_summary,
+                       is.numeric, round, 2),
+      aes(
+        label = glue::glue("{b_Intercept} [{.lower}, {.upper}]"),
+        y = label,
+        x = 1
+      ),
+      hjust = "inward",
+      size = 3,
+      color = "black"
+    ) +
+
+    scale_color_manual(values = c("#009E73", "#E69F00"), limits = rev) +
+    scale_fill_manual(values = c("#009E73", "#E69F00"), limits = rev) +
+
+    labs(
+      x = "Standardised Mean Difference (Positive Values Favour Self-Talk)",
+      # summary measure
+      y = element_blank(),
+      fill = "",
+      title = "Updated Posterior Pooled Estimate",
+      subtitle = "Prior and posterior distributions for pooled estimates, and mean and 95% quantile interval for posterior (text label)\nNote: x-axis rescaled from panel (A) for easier comparison of prior and posterior distributions"
+    ) +
+    scale_x_continuous(limits = c(-0.5, 1),
+                       breaks = c(-0.5, 0, 0.5, 1)) +
+    theme_classic() +
+    theme(
+      legend.position = "bottom",
+      panel.border = element_rect(fill = NA),
+      plot.subtitle = element_text(size = 6)
+    )
+
+  # Combine plots
+  (forest_study / posterior_update) +
+    plot_layout(heights = c(2, 1)) +
+    plot_annotation(tag_levels = "A")
+
+}
+
+fit_motor_demands_model <- function(data, prior) {
+  motor_demands_model <-
+    brm(
+      yi | se(sqrt(vi)) ~ 1 + motor_demands + (1 | study / experiment / group / effect),
+      data = data,
+      prior = prior,
+      chains = 4,
+      cores = 4,
+      seed = 1988,
+      warmup = 2000,
+      iter = 8000,
+      control = list(adapt_delta = 0.99)
+    )
+}
+
+
+
+
+# Model checks
+make_rhat_plot <- function(model) {
+  mod_rhat <- enframe(brms::rhat(model)) %>%
+    filter(!str_detect(name, "^r_id"))
+
+  rhat_main_params <- mod_rhat$value
+
+  mcmc_rhat(rhat_main_params) +
+    scale_x_continuous(breaks = c(1, 1.01, 1.02, 1.03, 1.04, 1.05)) +
+    geom_vline(xintercept = 1.01,
+               linetype = "dashed",
+               alpha = 0.25)
+}
+
+make_trace_plot <- function(model) {
+  plot(model)
+}
+
+make_pp_check <- function(model) {
+  pp_check(model)
+}
---
+++ _targets.R
@@ -0,0 +1,102 @@
+# _targets.R file
+library(targets)
+library(tarchetypes)
+source("R/functions.R")
+tar_option_set(
+  packages = c(
+    "here",
+    # "readxl",
+    "metafor",
+    "orchaRd",
+    "brms",
+    "modelr",
+    "tidybayes",
+    "bayesplot",
+    "rstan",
+    "ggridges",
+    "janitor",
+    "tidyverse",
+    "base",
+    # "scales",
+    # "ggtext",
+    # "zoo",
+    # "performance",
+    # "see",
+    # "lme4",
+    # "marginaleffects",
+    # "broom.mixed",
+    "patchwork"
+    # "kableExtra",
+    # "knitr",
+    # "bayestestR",
+    # "quarto",
+    # "officer",
+    # "officedown"
+  )
+)
+
+list(
+  # Load in and prepare data
+  tar_target(file, here("data","Final data.csv"), format = "file"),
+  tar_target(data, read_prepare_data(file)),
+  tar_target(data_effect_sizes, calculate_effect_sizes(data)),
+
+  # Fit, check, and plot main model
+  tar_target(rstan_setup, rstan_setup()), # to run chains in parallel
+  tar_target(intercept_prior, intercept_prior()),
+  tar_target(prior_plot, prior_plot()),
+  tar_target(main_model, fit_main_model(data_effect_sizes, intercept_prior)),
+  tar_target(rhat_main_model, make_rhat_plot(main_model)),
+  tar_target(trace_plot_main_model, make_trace_plot(main_model)),
+  tar_target(pp_check_main_model, make_pp_check(main_model)),
+  tar_target(main_model_plot, plot_main_model(data_effect_sizes, main_model)),
+
+  # Make plots tiffs
+  tar_target(prior_plot_tiff, make_plot_tiff(prior_plot, 7.5, 3.75, "plots/prior_plot.tiff")),
+  tar_target(main_model_plot_tiff, make_plot_tiff(main_model_plot, 7.5, 10, "plots/main_model_plot.tiff"))
+
+
+#
+#   # Model checks
+#   tar_target(model_checks, make_model_checks_tiff(model)),
+#
+#   # Make and save plots
+#   tar_target(individual_data_plot, plot_individual_data(data)),
+#   tar_target(individual_data_plot_tiff, make_individual_data_plot_tiff(individual_data_plot)),
+#
+#   tar_target(model_plot, plot_model(data, model)),
+#   tar_target(model_plot_tiff, make_model_plot_tiff(model_plot)),
+#
+#   tar_target(individual_preds_plot, plot_individual_preds(data, model)),
+#   tar_target(individual_preds_plot_tiff, make_individual_preds_plot_tiff(individual_preds_plot)),
+#
+#   tar_target(main_plot, combine_plots(individual_data_plot, individual_preds_plot, model_plot)),
+#
+#   # Add baseline adjustment to data
+#   tar_target(data_adj, add_adj_data(data)),
+#
+#   # Fit new model adjusting for each participants baseline
+#   tar_target(model_adj, fit_model_adj(data_adj)),
+#   tar_target(tidy_model_adj, get_tidy_model(model_adj)),
+#
+#   # Model checks
+#   tar_target(model_adj_checks, make_model_adj_checks_tiff(model_adj)),
+#
+#   # Make and save plots
+#
+#   tar_target(model_adj_plot, plot_model_adj(data_adj, model_adj)),
+#   tar_target(model_adj_plot_tiff, make_model_adj_plot_tiff(model_adj_plot)),
+#
+#   tar_target(individual_adj_preds_plot, plot_individual_adj_preds(data_adj, model_adj)),
+#   tar_target(individual_adj_preds_plot_tiff, make_individual_adj_preds_plot_tiff(individual_adj_preds_plot)),
+#
+#   tar_target(main_adj_plot, combine_adj_plots(individual_data_plot, individual_adj_preds_plot, model_adj_plot)),
+#
+#   # Compare models
+#   tar_target(model_comparison_2logBF, compare_models(model, model_adj)),
+#
+#   # Render the report
+#   tar_quarto(report, "report.qmd")
+#
+#
+)
